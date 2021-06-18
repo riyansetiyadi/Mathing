85df52a8c6842d9039c48c12232b23bd74310e80
WBX-doubleYunus <74029276+WBX-doubleYunus@users.noreply.github.com>
2021-06-18 15:37:59 +0700

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,38 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?= $data['titleHal'] ?> </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Mathing edu</title>
+    <link rel="stylesheet" href="fonts/font-awesome.css">
+    <link rel="stylesheet" href="fonts/google-fonts.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.js"></script>
 </head>
 <body>
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="<?= BASEURL; ?>">Mathing</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= BASEURL ?>/operator">Operator</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= BASEURL ?>/About">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= BASEURL ?>/profile/index/<?= $_SESSION["id"] ?>">My Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= BASEURL; ?>/profile/logout">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</div>
+  <!-- header section starts  -->
+<header id="header">
+<a href="#" class="logo"><img src="images/logo.png" alt=""></a>
+<nav>
+  <ul>
+    <li><a href="#home">home</a></li>
+    <li><a href="#Operator">Operator</a></li>
+    <li><a href="#About">About</a></li>
+    <li><a href="#My Profile">My Profile</a></li>
+    <li><a href="#contact">contact</a></li>
+  </ul>
+</nav>
+<div class="fas fa-bars"></div>
+</header>
