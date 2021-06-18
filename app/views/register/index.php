5996df3d2db93ba24cce0b7f8639626d9297d0f9
WBX-doubleYunus <74029276+WBX-doubleYunus@users.noreply.github.com>
2021-06-18 10:40:38 +0700

    Update index.php

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+	<div class="container">
 <form action="<?= BASEURL ?>/Register/registerPermission" method="post">
     <label for="username">Username </label>
     <input type="text" name="username">
@@ -6,3 +18,6 @@
     <button type="submit" name="register">Register</button>
 </form>
 <a href="<?= BASEURL ?>/Login">Login</a>
+</div>
+</body>
+</html>
