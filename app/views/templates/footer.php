3a000786b385ddda437b520a6dfb7eba8734afe6
WBX-doubleYunus <74029276+WBX-doubleYunus@users.noreply.github.com>
2021-06-18 15:36:26 +0700

    Update footer.php

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Mathing edu</title>
-    <link rel="stylesheet" href="fonts/font-awesome.css">
-    <link rel="stylesheet" href="fonts/google-fonts.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery.js"></script>
-</head>
-<body>
-  <!-- header section starts  -->
-<header id="header">
-<a href="#" class="logo"><img src="images/logo.png" alt=""></a>
-<nav>
-  <ul>
-    <li><a href="#home">home</a></li>
-    <li><a href="#Operator">Operator</a></li>
-    <li><a href="#About">About</a></li>
-    <li><a href="#My Profile">My Profile</a></li>
-    <li><a href="#contact">contact</a></li>
-  </ul>
-</nav>
-<div class="fas fa-bars"></div>
-</header>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
