d00a445ed89a487424be16280025383189e324bf
WBX-doubleYunus <74029276+WBX-doubleYunus@users.noreply.github.com>
2021-06-18 15:54:41 +0700

    Add files via upload

---
+++ app/views/home/index.php
@@ -0,0 +1,65 @@
+<div class="bg-home">
+    <div class="container">
+        <div class="jarak-navigasi"></div>
+        <div class="quote text-center">
+            Ilmu seperti udara <br>
+            Ia begitu banyak di sekeliling kita <br>
+            Kamu bisa mendapatkannya <br>
+            dimanapun dan kapanpun <br>
+            ~Socrates~
+        </div>
+    </div>
+</div>
+<div class="bg-white pt-5">
+    <div class="container pb-5">
+        <div class="grid-container">
+            <div class="card" style="width: 18rem;">
+                <img src="img/operator.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Operator</h5>
+                    <p class="card-text">Operator digunakan untuk melakukan operasi aritmatika seperti penjumlahan, pengurangan, pembagian, perkalian, dsb.</p>
+                    <a href="<?= BASEURL ?>/operator" class="btn btn-primary">Belajar sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/operator.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Operator</h5>
+                    <p class="card-text">Operator digunakan untuk melakukan operasi aritmatika seperti penjumlahan, pengurangan, pembagian, perkalian, dsb.</p>
+                    <a href="<?= BASEURL ?>/operator" class="btn btn-primary">Belajar sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/operator.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Operator</h5>
+                    <p class="card-text">Operator digunakan untuk melakukan operasi aritmatika seperti penjumlahan, pengurangan, pembagian, perkalian, dsb.</p>
+                    <a href="<?= BASEURL ?>/operator" class="btn btn-primary">Belajar sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/operator.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Operator</h5>
+                    <p class="card-text">Operator digunakan untuk melakukan operasi aritmatika seperti penjumlahan, pengurangan, pembagian, perkalian, dsb.</p>
+                    <a href="<?= BASEURL ?>/operator" class="btn btn-primary">Belajar sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/operator.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Operator</h5>
+                    <p class="card-text">Operator digunakan untuk melakukan operasi aritmatika seperti penjumlahan, pengurangan, pembagian, perkalian, dsb.</p>
+                    <a href="<?= BASEURL ?>/operator" class="btn btn-primary">Belajar sekarang</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about pt-5 pb-5" id="About">
+        <div class="container">
+            <h1 class="text-center mb-5">About Us</h1>
+            <p>Kami merupakan website belajar Matematika berbasis online.</p>
+            <p>Tujuan kami yaitu menyediakan media pembelajaran yang dapat diakses kapanpun dan dimanapun.</p>
+        </div>
+    </div>
+</div>
